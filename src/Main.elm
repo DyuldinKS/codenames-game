cda0fafc897fde9b7fcf70bb3435c69ed107c74a
Kirill Dyuldin <dyuldinks@gmail.com>
2021-10-24 15:43:57 +0300

    fix opening a card on touch deveices

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,7 @@ import Browser
 import Html exposing (Html, button, div, span, text)
 import Html.Attributes as Attr
 import Html.Events exposing (onClick, onDoubleClick)
+import Html.Events.Extra.Touch as Touch
 import Http
 import Json.Decode as D exposing (Decoder)
 import Json.Encode as E
@@ -32,6 +33,7 @@ type alias Model =
     , game : RData.WebData Game
     , role : Role
     , hasTouchScreen : Bool
+    , selectedWordId : WordId
     }
 
 
@@ -58,9 +60,14 @@ type alias WordId =
     Int
 
 
+noWordSelected : WordId
+noWordSelected =
+    -1
+
+
 initialState : Model
 initialState =
-    { gameId = Nothing, game = RData.NotAsked, role = NotDefined, hasTouchScreen = False }
+    { gameId = Nothing, game = RData.NotAsked, role = NotDefined, hasTouchScreen = False, selectedWordId = noWordSelected }
 
 
 init : String -> ( Model, Cmd Msg )
@@ -131,6 +138,7 @@ type Msg
     = StartGame
     | GetGameResponse (RData.WebData Game)
     | CreateGameResponse (RData.WebData Game)
+    | SelectWordToOpen WordId
     | OpenWordRequest WordId
     | OpenWordResponse WordId (RData.WebData ())
     | UpdateOpenedWords (List WordId)
@@ -175,8 +183,11 @@ update msg model =
                     Cmd.none
             )
 
+        SelectWordToOpen wordId ->
+            ( { model | selectedWordId = wordId }, Cmd.none )
+
         OpenWordRequest wordId ->
-            ( model
+            ( { model | selectedWordId = noWordSelected }
             , case model.game of
                 RData.Success game ->
                     apiOpenWord game.id wordId
@@ -282,7 +293,7 @@ view model =
                     text (httpErrorToString err)
 
                 RData.Success game ->
-                    viewScreenByRole model.role game
+                    viewScreenByRole model.role game model.hasTouchScreen model.selectedWordId
     in
     div [ Attr.class "layout" ] [ content ]
 
@@ -303,17 +314,17 @@ httpErrorToString err =
             "Unknown HTTP error"
 
 
-viewScreenByRole : Role -> Game -> Html Msg
-viewScreenByRole role game =
+viewScreenByRole : Role -> Game -> Bool -> WordId -> Html Msg
+viewScreenByRole role game hasTouchScreen selectedWordId =
     case role of
         NotDefined ->
             viewRoleSelectionScreen
 
         SimplePlayer ->
-            viewGame game False
+            viewGame game False hasTouchScreen selectedWordId
 
         Captain ->
-            viewGame game True
+            viewGame game True hasTouchScreen selectedWordId
 
 
 viewBtn : List (Html.Attribute msg) -> List (Html msg) -> Html msg
@@ -330,12 +341,12 @@ viewRoleSelectionScreen =
         ]
 
 
-viewGame : Game -> Bool -> Html Msg
-viewGame game isCaptain =
+viewGame : Game -> Bool -> Bool -> WordId -> Html Msg
+viewGame game isCaptain hasTouchScreen selectedWordId =
     div []
         [ div []
             [ div [ Attr.class "header" ] [ viewControls, viewGameId game.id, viewCounters game ] ]
-        , viewBoard game isCaptain
+        , viewBoard game isCaptain hasTouchScreen selectedWordId
         ]
 
 
@@ -391,18 +402,42 @@ countWordsLeft game teamWords =
         teamWords
 
 
-viewBoard : Game -> Bool -> Html Msg
-viewBoard game isCaptain =
-    div [ Attr.class "field" ] (List.indexedMap (viewWord game isCaptain) game.words)
+viewBoard : Game -> Bool -> Bool -> WordId -> Html Msg
+viewBoard game isCaptain hasTouchScreen selectedWordId =
+    div [ Attr.class "field" ] (List.indexedMap (viewWord game isCaptain hasTouchScreen selectedWordId) game.words)
 
 
-viewWord : Game -> Bool -> WordId -> Word -> Html Msg
-viewWord game isCaptain id word =
+onTouchStart : (Touch.Event -> msg) -> Html.Attribute msg
+onTouchStart =
+    { stopPropagation = False, preventDefault = False }
+        |> Touch.onWithOptions "touchstart"
+
+
+viewWord : Game -> Bool -> Bool -> WordId -> WordId -> Word -> Html Msg
+viewWord game isCaptain hasTouchScreen selectedWordId id word =
     div
         (List.append
-            [ Attr.classList <| getWordClassList id game isCaptain ]
+            [ Attr.classList <| getWordClassList id game isCaptain selectedWordId ]
             (if isWordOpened id game then
                 []
+
+             else if hasTouchScreen then
+                [ -- Touch.onEnd
+                  -- (\event ->
+                  --     let
+                  --         tmp =
+                  --             Debug.log "endEvent" event
+                  --     in
+                  --     SelectWordToOpen id
+                  -- )
+                  onTouchStart
+                    (if id == selectedWordId then
+                        \_ -> OpenWordRequest id
+
+                     else
+                        \_ -> SelectWordToOpen id
+                    )
+                ]
 
              else
                 [ onDoubleClick <| OpenWordRequest id ]
@@ -416,8 +451,8 @@ isWordOpened wordId game =
     Set.member wordId game.opened
 
 
-getWordClassList : WordId -> Game -> Bool -> List ( String, Bool )
-getWordClassList wordId game isCaptain =
+getWordClassList : WordId -> Game -> Bool -> WordId -> List ( String, Bool )
+getWordClassList wordId game isCaptain selectedWordId =
     List.append
         [ ( "card", True ) ]
         (if isCaptain || isWordOpened wordId game then
@@ -425,6 +460,9 @@ getWordClassList wordId game isCaptain =
             , ( getWordTeamClass wordId game, wordId /= game.fail )
             , ( "card--fail", wordId == game.fail )
             ]
+
+         else if wordId == selectedWordId then
+            [ ( "card--selected", True ) ]
 
          else
             []
